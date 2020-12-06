87efed60cd63127ae3c9740d8d2a13beecec9cc1
Christian Alexander <christian@linux.com>
2020-12-05 22:13:36 -0700

    solve 2020 day 6

--- 2020/lib/advent_of_code/day_06.ex
+++ 2020/lib/advent_of_code/day_06.ex
@@ -1,7 +1,29 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  def part1(args) do
+    String.split(args, "\n\n")
+    |> Enum.map(fn group ->
+      group
+      |> String.split("\n", trim: true)
+      |> Enum.flat_map(fn person ->
+        String.codepoints(person)
+      end)
+      |> MapSet.new()
+      |> Enum.count()
+    end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    String.split(args, "\n\n")
+    |> Enum.map(fn group ->
+      group
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn person ->
+        String.codepoints(person) |> MapSet.new()
+      end)
+      |> Enum.reduce(&MapSet.intersection(&1, &2))
+      |> Enum.count()
+    end)
+    |> Enum.sum()
   end
 end
--- 2020/test/advent_of_code/day_06_test.exs
+++ 2020/test/advent_of_code/day_06_test.exs
@@ -3,19 +3,51 @@ defmodule AdventOfCode.Day06Test do
 
   import AdventOfCode.Day06
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = """
+    abc
+
+    a
+    b
+    c
+
+    ab
+    ac
+
+    a
+    a
+    a
+    a
+
+    b
+    """
+
     result = part1(input)
 
-    assert result
+    assert result == 11
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = """
+    abc
+
+    a
+    b
+    c
+
+    ab
+    ac
+
+    a
+    a
+    a
+    a
+
+    b
+    """
+
     result = part2(input)
 
-    assert result
+    assert result == 6
   end
 end
