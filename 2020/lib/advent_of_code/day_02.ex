1159679def431d7b9c1c594c638096dd4a0ac3b2
Christian Alexander <christian@linux.com>
2020-12-06 21:47:31 -0700

    refactor(2020/day_02): use parser combinator

--- 2020/lib/advent_of_code/day_02.ex
+++ 2020/lib/advent_of_code/day_02.ex
@@ -1,32 +1,35 @@
 defmodule AdventOfCode.Day02 do
-  def part1(args) do
-    policy_regex = ~r/(?<min>\d+)-(?<max>\d+)\s(?<ch>.):\s(?<password>.*)/
+  defmodule Parser do
+    import NimbleParsec
 
+    range = integer(min: 1) |> ignore(string("-")) |> integer(min: 1)
+
+    password = repeat(ascii_char([?a..?z]))
+
+    defparsec(
+      :policy,
+      range
+      |> ignore(string(" "))
+      |> ascii_char([?a..?z])
+      |> ignore(string(": "))
+      |> concat(password)
+    )
+  end
+
+  def part1(args) do
     Stream.map(args, fn entry ->
-      Regex.named_captures(policy_regex, entry)
+      {:ok, [min, max, ch | password], _, _, _, _} = Parser.policy(entry)
+
+      %{min: min, max: max, ch: ch, password: password}
     end)
     |> Stream.filter(&evaluate_policy/1)
     |> Enum.count()
   end
 
-  def evaluate_policy(input) do
-    with {min, _} = Integer.parse(Map.fetch!(input, "min")),
-         {max, _} = Integer.parse(Map.fetch!(input, "max")) do
-      ch = Map.fetch!(input, "ch")
-      password = Map.fetch!(input, "password")
+  defp evaluate_policy(%{min: min, max: max, ch: ch, password: password}) do
+    c = Enum.count(password, &(&1 == ch))
 
-      c =
-        String.codepoints(password)
-        |> Enum.reduce(0, fn x, acc ->
-          if x == ch do
-            acc + 1
-          else
-            acc
-          end
-        end)
-
-      c >= min && c <= max
-    end
+    c >= min && c <= max
   end
 
   def part2(args) do
