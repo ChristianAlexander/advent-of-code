3a8675f02e0c5fa7b68712556f89c2eed462c945
Christian Alexander <christian@linux.com>
2020-12-07 22:36:45 -0700

    solve 2020 day 8

--- 2020/lib/mix/tasks/d08.p2.ex
+++ 2020/lib/mix/tasks/d08.p2.ex
@@ -5,7 +5,7 @@ defmodule Mix.Tasks.D08.P2 do
 
   @shortdoc "Day 08 Part 2"
   def run(args) do
-    input = nil
+    input = File.read!(Path.join(File.cwd!(), "lib/inputs/d08.txt"))
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
--- 2020/test/advent_of_code/day_08_test.exs
+++ 2020/test/advent_of_code/day_08_test.exs
@@ -3,19 +3,39 @@ defmodule AdventOfCode.Day08Test do
 
   import AdventOfCode.Day08
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = """
+    nop +0
+    acc +1
+    jmp +4
+    acc +3
+    jmp -3
+    acc -99
+    acc +1
+    jmp -4
+    acc +6
+    """
+
     result = part1(input)
 
-    assert result
+    assert result == 5
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = """
+    nop +0
+    acc +1
+    jmp +4
+    acc +3
+    jmp -3
+    acc -99
+    acc +1
+    jmp -4
+    acc +6
+    """
+
     result = part2(input)
 
-    assert result
+    assert result == 8
   end
 end
