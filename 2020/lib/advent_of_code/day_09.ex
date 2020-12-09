f9cde0aae6dbf803a209d255391e932c0e99a182
Christian Alexander <christian@linux.com>
2020-12-08 22:49:32 -0700

    solve 2020 day 9

--- 2020/lib/advent_of_code/day_09.ex
+++ 2020/lib/advent_of_code/day_09.ex
@@ -1,7 +1,75 @@
 defmodule AdventOfCode.Day09 do
-  def part1(_args) do
+  def part1(args, lookback) do
+    input =
+      String.split(args, "\n", trim: true)
+      |> Enum.map(fn x ->
+        {v, _} = Integer.parse(x)
+        v
+      end)
+
+    Enum.at(
+      input,
+      lookback..(Enum.count(input) - lookback)
+      |> Enum.find(fn skip_count ->
+        !set_contains_sum_to_value(
+          Enum.drop(input, skip_count - lookback)
+          |> Enum.take(lookback)
+          |> MapSet.new(),
+          Enum.at(input, skip_count)
+        )
+      end)
+    )
   end
 
-  def part2(_args) do
+  def set_contains_sum_to_value(set, value) do
+    Enum.any?(
+      set,
+      fn x ->
+        MapSet.member?(
+          MapSet.delete(set, value),
+          value - x
+        )
+      end
+    )
+  end
+
+  def part2(args, lookback) do
+    bad_entry = part1(args, lookback)
+
+    input =
+      String.split(args, "\n", trim: true)
+      |> Enum.map(fn x ->
+        {v, _} = Integer.parse(x)
+        v
+      end)
+
+    bad_entry_index = Enum.find_index(input, &(&1 == bad_entry))
+
+    (bad_entry_index - 1)..0
+    |> Task.async_stream(&find_range(input, &1, &1 + 1, bad_entry_index),
+      ordered: false
+    )
+    |> Stream.map(fn {:ok, x} -> x end)
+    |> Enum.find(&(&1 != nil))
+  end
+
+  @spec find_range(any, any, any, any) :: nil | number
+  def find_range(_, _, max, target) when max == target do
+    nil
+  end
+
+  def find_range(input, min, max, target) do
+    range = Enum.map(min..max, &Enum.at(input, &1))
+    sum = range |> Enum.sum()
+
+    if sum > Enum.at(input, target) do
+      nil
+    else
+      if sum == Enum.at(input, target) do
+        Enum.min(range) + Enum.max(range)
+      else
+        find_range(input, min, max + 1, target)
+      end
+    end
   end
 end
--- 2020/lib/mix/tasks/d09.p2.ex
+++ 2020/lib/mix/tasks/d09.p2.ex
@@ -5,13 +5,13 @@ defmodule Mix.Tasks.D09.P2 do
 
   @shortdoc "Day 09 Part 2"
   def run(args) do
-    input = nil
+    input = File.read!(Path.join(File.cwd!(), "lib/inputs/d09.txt"))
 
     if Enum.member?(args, "-b"),
-      do: Benchee.run(%{part_2: fn -> input |> part2() end}),
+      do: Benchee.run(%{part_2: fn -> input |> part2(25) end}),
       else:
         input
-        |> part2()
+        |> part2(25)
         |> IO.inspect(label: "Part 2 Results")
   end
 end
