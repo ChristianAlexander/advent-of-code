97008d902f627ffbadcb81ae65c0d06d44d46e01
Christian Alexander <christian@linux.com>
2021-11-30 22:23:34 -0700

    Live solve day 1

--- 2021/lib/advent_of_code/day_01.ex
+++ 2021/lib/advent_of_code/day_01.ex
@@ -1,9 +1,35 @@
 defmodule AdventOfCode.Day01 do
   def part1(args) do
-    args
+    args = args |> AdventOfCode.Load.numeric_lines() |> Enum.into([])
+
+    [_head | rest] = args
+
+    Enum.zip(args, rest) |> Enum.filter(fn {a, b} -> a < b end) |> Enum.count()
   end
 
   def part2(args) do
-    args
+    args = args |> AdventOfCode.Load.numeric_lines() |> Enum.into([])
+
+    x =
+      args
+      |> Enum.with_index()
+      |> Enum.map(fn {_, index} ->
+        get_three_sums(index, args)
+      end)
+      |> Enum.filter(&Function.identity/1)
+
+    [_head | rest] = x
+
+    Enum.zip(x, rest) |> Enum.filter(fn {a, b} -> a < b end) |> Enum.count()
+  end
+
+  def get_three_sums(position, args) do
+    x = args |> Enum.slice(position..-1) |> Enum.take(3)
+
+    if Enum.count(x) < 3 do
+      nil
+    else
+      Enum.sum(x)
+    end
   end
 end
