962aff797ca42a5ddd85b79fabd5039a9a259ed2
Christian Alexander <christian@linux.com>
2021-12-02 22:32:57 -0700

    refactor day 3

--- 2021/lib/advent_of_code/day_03.ex
+++ 2021/lib/advent_of_code/day_03.ex
@@ -3,41 +3,17 @@ defmodule AdventOfCode.Day03 do
     inputs =
       args
       |> AdventOfCode.Load.lines()
-
-    counted_values =
-      inputs
       |> Enum.map(&String.graphemes/1)
-      |> Enum.zip()
-      |> Enum.map(fn digits ->
-        digits
-        |> Tuple.to_list()
-        |> Enum.reduce({0, 0}, fn
-          "0", {zeros, ones} -> {zeros + 1, ones}
-          "1", {zeros, ones} -> {zeros, ones + 1}
-        end)
-      end)
 
     gamma =
-      counted_values
-      |> Enum.map(fn
-        {zeros, ones} when zeros > ones ->
-          "0"
-
-        _ ->
-          "1"
-      end)
+      inputs
+      |> most_common_per_position()
       |> Enum.join()
       |> String.to_integer(2)
 
     epsilon =
-      counted_values
-      |> Enum.map(fn
-        {zeros, ones} when zeros < ones ->
-          "0"
-
-        _ ->
-          "1"
-      end)
+      inputs
+      |> least_common_per_position()
       |> Enum.join()
       |> String.to_integer(2)
 
@@ -48,56 +24,55 @@ defmodule AdventOfCode.Day03 do
     inputs =
       args
       |> AdventOfCode.Load.lines()
+      |> Enum.map(&String.graphemes/1)
 
     oxygen =
       inputs
-      |> filter_common_numbers(&get_common_values_per_position/1)
-      |> String.to_integer(2)
+      |> reduce_masked_numbers(&most_common_per_position/1)
 
     co2 =
       inputs
-      |> filter_common_numbers(&get_uncommon_values_per_position/1)
-      |> String.to_integer(2)
+      |> reduce_masked_numbers(&least_common_per_position/1)
 
     oxygen * co2
   end
 
-  defp filter_common_numbers(input_numbers, get_values, place \\ 0) do
-    common_value = get_values.(input_numbers) |> Enum.at(place)
+  defp reduce_masked_numbers(candidates, get_mask, position \\ 0)
 
-    filtered_numbers =
-      input_numbers
-      |> Enum.filter(fn num ->
-        value_in_place =
-          num
-          |> String.graphemes()
-          |> Enum.at(place)
-
-        value_in_place == common_value
-      end)
-
-    if filtered_numbers |> Enum.count() == 1 do
-      hd(filtered_numbers)
-    else
-      filter_common_numbers(filtered_numbers, get_values, place + 1)
-    end
+  defp reduce_masked_numbers([candidate], _, _) do
+    candidate
+    |> Enum.join()
+    |> String.to_integer(2)
   end
 
-  defp get_common_values_per_position(input_numbers) do
-    counted_values =
-      input_numbers
-      |> Enum.map(&String.graphemes/1)
-      |> Enum.zip()
-      |> Enum.map(fn digits ->
-        digits
-        |> Tuple.to_list()
-        |> Enum.reduce({0, 0}, fn
-          "0", {zeros, ones} -> {zeros + 1, ones}
-          "1", {zeros, ones} -> {zeros, ones + 1}
-        end)
-      end)
+  defp reduce_masked_numbers(candidates, get_mask, position) do
+    mask_at_position =
+      get_mask.(candidates)
+      |> Enum.at(position)
 
-    counted_values
+    filtered_candidates =
+      candidates
+      |> Enum.filter(&(Enum.at(&1, position) == mask_at_position))
+
+    reduce_masked_numbers(filtered_candidates, get_mask, position + 1)
+  end
+
+  defp count_values_in_positions(input_numbers) do
+    input_numbers
+    |> Enum.zip()
+    |> Enum.map(fn digits ->
+      digits
+      |> Tuple.to_list()
+      |> Enum.reduce({0, 0}, fn
+        "0", {zeros, ones} -> {zeros + 1, ones}
+        "1", {zeros, ones} -> {zeros, ones + 1}
+      end)
+    end)
+  end
+
+  defp least_common_per_position(input_numbers) do
+    input_numbers
+    |> count_values_in_positions
     |> Enum.map(fn
       {zeros, ones} when zeros > ones ->
         "0"
@@ -107,21 +82,9 @@ defmodule AdventOfCode.Day03 do
     end)
   end
 
-  defp get_uncommon_values_per_position(input_numbers) do
-    counted_values =
-      input_numbers
-      |> Enum.map(&String.graphemes/1)
-      |> Enum.zip()
-      |> Enum.map(fn digits ->
-        digits
-        |> Tuple.to_list()
-        |> Enum.reduce({0, 0}, fn
-          "0", {zeros, ones} -> {zeros + 1, ones}
-          "1", {zeros, ones} -> {zeros, ones + 1}
-        end)
-      end)
-
-    counted_values
+  defp most_common_per_position(input_numbers) do
+    input_numbers
+    |> count_values_in_positions
     |> Enum.map(fn
       {zeros, ones} when zeros > ones ->
         "1"
