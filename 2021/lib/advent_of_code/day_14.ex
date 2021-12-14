fd6273c6af9a98bd18bf951ebd25fc150baed53f
Christian Alexander <christian@linux.com>
2021-12-13 23:07:06 -0700

    solve day 14

--- 2021/lib/advent_of_code/day_14.ex
+++ 2021/lib/advent_of_code/day_14.ex
@@ -1,7 +1,72 @@
 defmodule AdventOfCode.Day14 do
   def part1(args) do
+    {pairs, substitutions, final_template_char} = parse(args)
+
+    1..10
+    |> Enum.reduce(pairs, fn _, pairs ->
+      expand(pairs, substitutions)
+    end)
+    |> get_character_frequencies(final_template_char)
+    |> calculate_score()
   end
 
   def part2(args) do
+    {pairs, substitutions, final_template_char} = parse(args)
+
+    1..40
+    |> Enum.reduce(pairs, fn _, pairs ->
+      expand(pairs, substitutions)
+    end)
+    |> get_character_frequencies(final_template_char)
+    |> calculate_score()
+  end
+
+  defp parse(args) do
+    [template | substitutions] = args |> AdventOfCode.Load.lines()
+
+    template = String.graphemes(template)
+
+    substitutions =
+      substitutions
+      |> Enum.map(fn substitution ->
+        [pair, result] = String.split(substitution, " -> ")
+        [a, b] = String.graphemes(pair)
+
+        {{a, b}, result}
+      end)
+      |> Enum.into(Map.new())
+
+    pairs =
+      template
+      |> Enum.zip(Enum.drop(template, 1))
+      |> Enum.reduce(Map.new(), fn pair, acc ->
+        Map.update(acc, pair, 1, &(&1 + 1))
+      end)
+
+    {pairs, substitutions, List.last(template)}
+  end
+
+  defp expand(template_pair_counts, substitutions) do
+    template_pair_counts
+    |> Enum.reduce(Map.new(), fn {{a, b} = pair, count}, acc ->
+      sub = Map.get(substitutions, pair)
+
+      Map.update(acc, {a, sub}, count, &(&1 + count))
+      |> Map.update({sub, b}, count, &(&1 + count))
+    end)
+  end
+
+  defp get_character_frequencies(pairs, final_template_char) do
+    pairs
+    |> Enum.map(fn {{a, _}, count} -> {a, count} end)
+    |> Enum.reduce(Map.new(), fn
+      {x, count}, acc -> Map.update(acc, x, count, &(&1 + count))
+    end)
+    |> Map.update(final_template_char, 1, &(&1 + 1))
+  end
+
+  defp calculate_score(character_frequencies) do
+    Enum.max(character_frequencies |> Map.values()) -
+      Enum.min(character_frequencies |> Map.values())
   end
 end
--- 2021/test/advent_of_code/day_14_test.exs
+++ 2021/test/advent_of_code/day_14_test.exs
@@ -3,19 +3,38 @@ defmodule AdventOfCode.Day14Test do
 
   import AdventOfCode.Day14
 
-  @tag :skip
+  @shared_input """
+  NNCB
+
+  CH -> B
+  HH -> N
+  CB -> H
+  NH -> C
+  HB -> C
+  HC -> B
+  HN -> C
+  NN -> C
+  BH -> H
+  NC -> B
+  NB -> B
+  BN -> B
+  BB -> N
+  BC -> B
+  CC -> N
+  CN -> C
+  """
+
   test "part1" do
-    input = nil
+    input = @shared_input
     result = part1(input)
 
-    assert result
+    assert result == 1588
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = @shared_input
     result = part2(input)
 
-    assert result
+    assert result == 2_188_189_693_529
   end
 end
