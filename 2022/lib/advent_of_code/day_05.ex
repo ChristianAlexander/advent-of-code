c5472af7f1fe3f553e222f576110460306835575
Christian Alexander <christian@linux.com>
2022-12-04 22:41:33 -0700

    feat: solve 2022-05

--- 2022/lib/advent_of_code/day_05.ex
+++ 2022/lib/advent_of_code/day_05.ex
@@ -1,7 +1,113 @@
 defmodule AdventOfCode.Day05 do
+  defmodule Parser do
+    import NimbleParsec
+
+    crate =
+      ignore(string("["))
+      |> ascii_char([?A..?Z])
+      |> ignore(string("]"))
+      |> optional(ignore(string(" ")))
+
+    empty_space = string("   ") |> optional(string(" ")) |> replace(:empty)
+
+    defparsecp(:inner_parse, repeat(choice([crate, empty_space])))
+
+    instruction =
+      ignore(string("move "))
+      |> integer(min: 1, max: 2)
+      |> ignore(string(" from "))
+      |> integer(1)
+      |> ignore(string(" to "))
+      |> integer(1)
+
+    defparsecp(:inner_parse_instruction, instruction)
+
+    def parse(line) do
+      {:ok, results, _, _, _, _} = inner_parse(line)
+
+      results
+    end
+
+    def parse_instruction(line) do
+      {:ok, results, _, _, _, _} = inner_parse_instruction(line)
+
+      results
+    end
+  end
+
   def part1(args) do
+    args = AdventOfCode.Load.lines(args)
+
+    {definitions, instructions} = Enum.split_while(args, &(not String.starts_with?(&1, " 1")))
+
+    box_definitions =
+      definitions
+      |> Enum.map(&Parser.parse/1)
+      |> Enum.flat_map(&Enum.with_index/1)
+      |> Enum.reduce(%{}, fn
+        {:empty, _}, acc ->
+          acc
+
+        {val, idx}, acc ->
+          Map.update(acc, idx + 1, [val], fn existing ->
+            Enum.concat(existing, [val])
+          end)
+      end)
+
+    instructions =
+      instructions
+      |> Enum.drop(1)
+      |> Enum.map(&Parser.parse_instruction/1)
+
+    results =
+      Enum.reduce(instructions, box_definitions, fn [count, source, destination], definitions ->
+        {items, source_tail} = Enum.split(Map.get(definitions, source), count)
+
+        definitions
+        |> Map.update!(destination, fn dest_vals ->
+          Enum.concat(Enum.reverse(items), dest_vals)
+        end)
+        |> Map.put(source, source_tail)
+      end)
+
+    results |> Enum.map(fn {_, v} -> hd(v) end) |> List.to_string()
   end
 
   def part2(args) do
+    args = AdventOfCode.Load.lines(args)
+
+    {definitions, instructions} = Enum.split_while(args, &(not String.starts_with?(&1, " 1")))
+
+    box_definitions =
+      definitions
+      |> Enum.map(&Parser.parse/1)
+      |> Enum.flat_map(&Enum.with_index/1)
+      |> Enum.reduce(%{}, fn
+        {:empty, _}, acc ->
+          acc
+
+        {val, idx}, acc ->
+          Map.update(acc, idx + 1, [val], fn existing ->
+            Enum.concat(existing, [val])
+          end)
+      end)
+
+    instructions =
+      instructions
+      |> Enum.drop(1)
+      |> Enum.map(&Parser.parse_instruction/1)
+
+    results =
+      Enum.reduce(instructions, box_definitions, fn [count, source, destination], definitions ->
+        {items, source_tail} = Enum.split(Map.get(definitions, source), count)
+
+        definitions
+        |> Map.update!(destination, fn dest_vals ->
+          Enum.concat(items, dest_vals)
+        end)
+        |> Map.put(source, source_tail)
+      end)
+
+    results |> Enum.map(fn {_, v} -> hd(v) end) |> List.to_string()
   end
 end
--- 2022/test/advent_of_code/day_05_test.exs
+++ 2022/test/advent_of_code/day_05_test.exs
@@ -3,19 +3,29 @@ defmodule AdventOfCode.Day05Test do
 
   import AdventOfCode.Day05
 
-  @tag :skip
+  @shared_input """
+      [D]
+  [N] [C]
+  [Z] [M] [P]
+   1   2   3
+
+  move 1 from 2 to 1
+  move 3 from 1 to 3
+  move 2 from 2 to 1
+  move 1 from 1 to 2
+  """
+
   test "part1" do
-    input = nil
+    input = @shared_input
     result = part1(input)
 
-    assert result
+    assert result == "CMZ"
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = @shared_input
     result = part2(input)
 
-    assert result
+    assert result == "MCD"
   end
 end
