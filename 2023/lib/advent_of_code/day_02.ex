7d8eab11e12a99174388a155c8f4d53dd9c6b377
Christian Alexander <christian@linux.com>
2023-12-02 15:19:01 -0700

    solve 2023 day 2

--- 2023/lib/advent_of_code/day_02.ex
+++ 2023/lib/advent_of_code/day_02.ex
@@ -1,7 +1,87 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  defmodule Game do
+    defstruct [:id, :rounds]
   end
 
-  def part2(_args) do
+  defmodule Parser do
+    import NimbleParsec
+
+    game_id = ignore(string("Game ")) |> integer(min: 1) |> ignore(string(": "))
+
+    color =
+      choice([
+        replace(string("red"), :red),
+        replace(string("green"), :green),
+        replace(string("blue"), :blue)
+      ])
+
+    cube = integer(min: 1) |> ignore(string(" ")) |> concat(color) |> reduce(:transform_cube)
+
+    defparsecp(
+      :game_id,
+      game_id
+    )
+
+    defparsecp(:cube_list, repeat(cube |> ignore(optional(string(", ")))))
+
+    defp transform_cube([count, color]) do
+      {color, count}
+    end
+
+    defp parse_round(round_string) do
+      {:ok, round, _, _, _, _} = cube_list(round_string)
+
+      round
+    end
+
+    def parse(line) do
+      {:ok, [id], rounds, _, _, _} = game_id(line)
+
+      rounds =
+        rounds
+        |> String.split("; ")
+        |> Enum.map(&parse_round/1)
+
+      %Game{id: id, rounds: rounds}
+    end
+  end
+
+  def part1(args) do
+    lines = AdventOfCode.Load.lines(args)
+
+    lines
+    |> Enum.map(&Parser.parse/1)
+    |> Enum.filter(fn %{rounds: rounds} ->
+      rounds
+      |> List.flatten()
+      |> Enum.all?(fn {color, count} ->
+        cond do
+          color == :red && count > 12 -> false
+          color == :green && count > 13 -> false
+          color == :blue && count > 14 -> false
+          true -> true
+        end
+      end)
+    end)
+    |> Enum.map(fn %{id: id} -> id end)
+    |> Enum.sum()
+  end
+
+  def part2(args) do
+    lines = AdventOfCode.Load.lines(args)
+
+    lines
+    |> Enum.map(&Parser.parse/1)
+    |> Enum.map(fn %{rounds: rounds} ->
+      rounds
+      |> List.flatten()
+      |> Enum.reduce(%{}, fn {color, count}, acc ->
+        Map.update(acc, color, count, &max(count, &1))
+      end)
+      |> Enum.reduce(1, fn {_color, count}, acc ->
+        acc * count
+      end)
+    end)
+    |> Enum.sum()
   end
 end
--- 2023/test/advent_of_code/day_02_test.exs
+++ 2023/test/advent_of_code/day_02_test.exs
@@ -3,19 +3,23 @@ defmodule AdventOfCode.Day02Test do
 
   import AdventOfCode.Day02
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @shared_input """
+  Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+  Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+  Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+  Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+  Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+  """
 
-    assert result
+  test "part1" do
+    result = part1(@shared_input)
+
+    assert result == 8
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    result = part2(@shared_input)
 
-    assert result
+    assert result == 2286
   end
 end
