37288a9b7741e0772f87dea74b9046fcb9dbd27a
Christian Alexander <christian@linux.com>
2023-12-05 21:41:56 -0700

    solve 2023 day 5

--- 2023/lib/advent_of_code/day_05.ex
+++ 2023/lib/advent_of_code/day_05.ex
@@ -1,7 +1,144 @@
 defmodule AdventOfCode.Day05 do
-  def part1(__args) do
+  defmodule Parser do
+    import NimbleParsec
+
+    seeds = ignore(string("seeds: ")) |> repeat(choice([integer(min: 1), ignore(string(" "))]))
+
+    mapping_header =
+      unwrap_and_tag(ascii_string([?a..?z], min: 1), :source)
+      |> ignore(string("-to-"))
+      |> unwrap_and_tag(ascii_string([?a..?z], min: 1), :destination)
+      |> ignore(string(" map:\n"))
+
+    mapping =
+      unwrap_and_tag(integer(min: 1), :destination)
+      |> ignore(string(" "))
+      |> unwrap_and_tag(integer(min: 1), :source_start)
+      |> ignore(string(" "))
+      |> unwrap_and_tag(integer(min: 1), :source_range)
+
+    defparsecp(
+      :seed_parser,
+      seeds
+    )
+
+    defparsecp(
+      :mapping_parser,
+      mapping_header |> repeat(choice([tag(mapping, :mapping), ignore(string("\n"))]))
+    )
+
+    defp parse_mapping(mapping) do
+      {:ok, results, _, _, _, _} = mapping_parser(mapping)
+
+      source = Keyword.get(results, :source)
+      destination = Keyword.get(results, :destination)
+
+      mappings =
+        Keyword.get_values(results, :mapping)
+        |> Enum.sort_by(fn [_dest, start, _range] -> start end)
+        |> Enum.map(fn [
+                         destination: destination,
+                         source_start: source_start,
+                         source_range: source_range
+                       ] ->
+          {source_start..(source_start + source_range - 1), destination}
+        end)
+
+      {source, {destination, mappings}}
+    end
+
+    def parse([seed_data | mappings]) do
+      {:ok, seeds, _, _, _, _} = seed_parser(seed_data)
+
+      seeds = Enum.map(seeds, fn seed -> {"seed", seed} end)
+
+      mappings =
+        Enum.map(mappings, &parse_mapping/1)
+        |> Enum.into(%{})
+
+      {seeds, mappings}
+    end
   end
 
-  def part2(__args) do
+  defp get_location({"location", value}, _mappings), do: value
+
+  defp get_location({source, value}, mappings) do
+    {destination, dest_mappings} = Map.get(mappings, source)
+
+    next_value =
+      Enum.find_value(dest_mappings, value, fn {start.._end = range, destination} ->
+        if value in range do
+          destination + (value - start)
+        else
+          nil
+        end
+      end)
+
+    get_location({destination, next_value}, mappings)
+  end
+
+  def part1(args) do
+    {seeds, mappings} =
+      args
+      |> String.split("\n\n", trim: true)
+      |> Parser.parse()
+
+    Enum.map(seeds, &get_location(&1, mappings))
+    |> Enum.min()
+  end
+
+  def map_ranges(x..x, _, accumulated_ranges) do
+    accumulated_ranges
+  end
+
+  def map_ranges(remaining_range, [], accumulated_ranges) do
+    [remaining_range | accumulated_ranges]
+  end
+
+  def map_ranges(x..y, [{_a..b, _dest} | rest], acc) when x >= b, do: map_ranges(x..y, rest, acc)
+
+  def map_ranges(x..y, [{a.._b, _dest} | _] = possible_ranges, acc) when x < a do
+    map_ranges(min(a, y)..y, possible_ranges, [x..(a - 1) | acc])
+  end
+
+
+  def map_ranges(x..y, [{a..b, dest} | _] = possible_ranges, acc) when x == a do
+    dest_mapped_range = dest..(dest + (min(b, y) - a))
+
+
+    map_ranges(min(b + 1, y)..y, possible_ranges, [dest_mapped_range | acc])
+  end
+
+  def map_ranges(x..y, [{a..b, dest} | _ ] = possible_ranges, acc) when x > a do
+    dest_mapped_range = (x - a) + dest..(dest + (min(b, y) - a))
+
+    map_ranges(min(b + 1, y)..y, possible_ranges, [dest_mapped_range | acc])
+  end
+
+  defp map_source_to_destinations({"location", ranges}, _mappings), do: ranges
+
+  defp map_source_to_destinations({source, ranges}, mappings) do
+    {destination, dest_mappings} = Map.get(mappings, source)
+
+    mapped_ranges = Enum.flat_map(ranges, fn range ->
+      candidate_mappings = Enum.filter(dest_mappings, fn {source_range, _} -> not Range.disjoint?(source_range, range) end)
+      map_ranges(range, candidate_mappings, []) end)
+
+    map_source_to_destinations({destination, mapped_ranges}, mappings)
+  end
+
+  def part2(args) do
+    {seeds, mappings} =
+      args
+      |> String.split("\n\n", trim: true)
+      |> Parser.parse()
+
+      seed_ranges =
+    Enum.chunk_every(seeds, 2)
+    |> Enum.map(fn [{_, first}, {_, length}] -> first..(first + length - 1) end)
+
+    map_source_to_destinations({"seed", seed_ranges}, mappings)
+    |> Enum.map(&(&1.first))
+    |> Enum.min()
   end
 end
--- 2023/test/advent_of_code/day_05_test.exs
+++ 2023/test/advent_of_code/day_05_test.exs
@@ -3,19 +3,67 @@ defmodule AdventOfCode.Day05Test do
 
   import AdventOfCode.Day05
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @shared_input """
+  seeds: 79 14 55 13
 
-    assert result
+  seed-to-soil map:
+  50 98 2
+  52 50 48
+
+  soil-to-fertilizer map:
+  0 15 37
+  37 52 2
+  39 0 15
+
+  fertilizer-to-water map:
+  49 53 8
+  0 11 42
+  42 0 7
+  57 7 4
+
+  water-to-light map:
+  88 18 7
+  18 25 70
+
+  light-to-temperature map:
+  45 77 23
+  81 45 19
+  68 64 13
+
+  temperature-to-humidity map:
+  0 69 1
+  1 0 69
+
+  humidity-to-location map:
+  60 56 37
+  56 93 4
+  """
+
+  test "part1" do
+    result = part1(@shared_input)
+
+    assert result == 35
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  test "map_ranges 0..10" do
+    result =
+      map_ranges(0..10, [{2..4, 10}, {7..8, 20}], [])
+      |> Enum.sort()
 
-    assert result
+    assert result == [0..1, 10..12, 5..6, 20..21, 9..10] |> Enum.sort()
+  end
+
+  test "map_ranges mid" do
+    result =
+      map_ranges(3..15, [{2..4, 10}, {7..8, 20}], [])
+      |> Enum.sort()
+
+    assert result == [11..12, 5..6, 20..21, 9..15] |> Enum.sort()
+  end
+
+  test "part2" do
+    result = part2(@shared_input)
+
+    assert result == 46
   end
 end
